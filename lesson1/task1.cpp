58425cf89c6279a3ad896e40e021aa1cac5247b8
atlas <atlaso4ek@gmail.com>
2025-11-24 18:52:18 +0700

    l1 t1

---
+++ lesson1/task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Выполнил Кравченко Алексей 9КС-232" << endl;
+    
+    double a, b;
+
+    cout << "Введите 2 числа " << endl;
+    cin >> a >> b;
+
+    cout << "Сумма: " << a + b << endl;
+    cout << "Разность: " << a - b << endl;
+    cout << "Произведение: " << a * b << endl;
+    cout << "Частное: " << a / b << endl;
+
+    return 0;
+}
