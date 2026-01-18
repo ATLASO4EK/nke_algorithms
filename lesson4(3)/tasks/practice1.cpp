8219c492c194ef7a4ba97ae997017274a577f6f7
atlas <atlaso4ek@gmail.com>
2026-01-18 22:19:26 +0700

    lesson4(3)

---
+++ lesson4(3)/tasks/practice1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    cout << "Выполнил Кравченко Алексей 9КС-232" << endl;
+    int i, n;
+    int sum = 0;
+    cout << "Введите число" << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++){
+        sum += i;
+    }
+    cout << "The sum (for): " << sum << endl;
+
+    sum = 0;
+    i = 1;
+    while (i <= n) {
+        sum = sum + i;
+        i++;
+    }
+    cout << "The sum (while): " << sum << endl;
+
+    sum = 0;
+    i = 1;
+    if (i < n){
+        do {
+            sum = sum + i;
+            i++;
+        } while(i<=n);
+    }
+    cout << "The sum (do while): " << sum << endl;
+
+    sum = (n*(n+1))/2;
+    cout << "The sum (no loop): " << sum << endl;
+
+
+    return 0;
+}
